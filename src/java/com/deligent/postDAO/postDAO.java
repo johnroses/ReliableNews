cdf2ee56676c83a9a0f1e6da905088929091ae11
Deligent <Deligent@DELIGENT>
2018-04-02 20:14:30 +0530

    Post DAO methods were added

--- src/java/com/deligent/postDAO/postDAO.java
+++ src/java/com/deligent/postDAO/postDAO.java
@@ -5,11 +5,15 @@
  */
 package com.deligent.postDAO;
 
+
 import com.deligent.postDTO.PostDTO;
+import java.util.List;
+import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,7 +33,7 @@ public class postDAO {
     public void insertPost(PostDTO usr) {
 
         
-        System.out.println(" DAO ===============");
+        System.out.println(" POST DAO ===============");
         Session session = sessionFactory.openSession();
         Transaction tx = null;
 
@@ -52,9 +56,65 @@ public class postDAO {
 
     }
     
-    public void viewPost(PostDTO post){
+    @Transactional
+    public List<PostDTO> viewWallPost(){
+        
+    
+        Session session=sessionFactory.openSession();
+        Transaction tx=null;
+        List<PostDTO> mymsg=null;
+         try{
+                tx=session.beginTransaction();
+                                   
+
+                mymsg = session.createQuery("from PostDTO").list();
+               // Iterator next=(Iterator) user1;
+               int n=mymsg.size();
+               System.out.println("n "+n);
+
+                tx.commit();
+        }catch(HibernateException e){
+            e.printStackTrace();
+        }
+
+         return mymsg;
         
     }
+
+    
+    @Transactional
+    public List<PostDTO> filterViewWallPost(PostDTO postDTO){
+        
+    
+        Session session=sessionFactory.openSession();
+        Transaction tx=null;
+        List<PostDTO> mymsg=null;
+        List<PostDTO> post=null;
+         try{
+                tx=session.beginTransaction();
+                                   
+                //user1 = session.createCriteria("from UserDtO").list();
+                mymsg = session.createQuery("from PostDTO").list();
+                
+                Criteria cri = session.createCriteria(PostDTO.class);
+                String g[]={postDTO.getGroup1(),postDTO.getGroup2()};
+                cri.add(Restrictions.in("group1", g));
+               // Iterator next=(Iterator) user1;
+               
+                       post=(List<PostDTO>) cri.uniqueResult();
+               int n=mymsg.size();
+               System.out.println("n "+n);
+
+                //tx.commit();
+                session.getTransaction().commit();
+        }catch(HibernateException e){
+            e.printStackTrace();
+        }
+         return post;
+        
+    }
+    
+    
 
     
 }
