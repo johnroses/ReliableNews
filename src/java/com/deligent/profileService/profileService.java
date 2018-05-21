87d362e891334654cad9346af51a7510a5487e83
Deligent <Deligent@DELIGENT>
2018-05-21 08:31:12 +0530

    update_non_support_Profile, update_neutral methods are added

--- src/java/com/deligent/profileService/profileService.java
+++ src/java/com/deligent/profileService/profileService.java
@@ -16,32 +16,43 @@ import org.springframework.transaction.annotation.Transactional;
  *
  * @author Deligent
  */
-
 @Service
 @Transactional
 public class profileService {
+
     @Autowired
     private profileDAO profiledao;
-    
+
     @Transactional
-     public void insertProfile(profileDTO profile) {
+    public void insertProfile(profileDTO profile) {
         profiledao.insertProfile(profile);
     }
-     
-     public List<profileDTO> viewProfile(profileDTO profile){
-         return profiledao.viewProfile(profile);        
-     }
-     @Transactional
-     public void updateProfile(profileDTO profile){
-     //public List<profileDTO> updateProfile(profileDTO profile){
-         //return
-                 profiledao.updateProfile(profile);        
-     }
 
-     @Transactional
-    public void updateProfile1(profileDTO post1) {
-        profiledao.updateProfile1(post1);
+    public List<profileDTO> viewProfile(profileDTO profile) {
+        return profiledao.viewProfile(profile);
+    }
+
+    @Transactional
+    public void updateProfile(String group1, String group2, profileDTO profile) {
+        //public List<profileDTO> updateProfile(profileDTO profile){
+        //return
+        profiledao.updateProfile2(group1, group2, profile);
+    }
+
+    @Transactional
+    public void updateProfile1(String group1, String group2, profileDTO post1) {
+        profiledao.updateProfile2(group1, group2, post1);
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+    @Transactional
+    public void update_non_support_Profile(String group1, String group2, profileDTO post1){
+        profiledao.update_non_support_Profile(group1, group2, post1);
+        
+    }
+    @Transactional
+    public void update_neutral_Profile(String group1, String group2, profileDTO post1){
+        profiledao.update_neutral_Profile(group1, group2, post1);
+        
+    }
+
 }
